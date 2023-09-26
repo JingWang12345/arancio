0cec8800f56446aca3972086fbc850baa6eae676
JingWang <wangjing061993@gmail.com>
2023-09-26 23:41:13 +0200

    undate

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ loadAranci();
 
 function renderPage(data) {
     console.log(data);
-    const aranciContainer= document.getElementById('aranci-container');
-    aranciContainer.innerHTML='';
+    const aranciContainer = document.getElementById('aranci-container');
+    aranciContainer.innerHTML = '';
     for (let i = 0; i < data.length; i++) {
         const arancio = data[i];
         const container = document.createElement('div');
@@ -17,10 +17,36 @@ function renderPage(data) {
                 <p>Capital: ${arancio.capital}</p>
                 <p>Population: ${arancio.population}</p>
                 <p>Official: ${arancio.name.official}</p>
+                <div>${arancio.flag}</div>
                  </div>
             `
-            container.innerHTML=template;
-            aranciContainer.appendChild(container);
-     
+        container.innerHTML = template;
+        aranciContainer.appendChild(container);
+
+    }
 }
+
+function orderName() {
+ 
+    const data = arancio.name.common;
+
+    // Ordina l'array per il campo 'name' in ordine alfabetico crescente
+    data.sort((a, b) => {
+        const nameA = a.name.toLowerCase(); // Converte i nomi in minuscolo per l'ordinamento senza distinzione tra maiuscole e minuscole
+        const nameB = b.name.toLowerCase();
+        return nameA.localeCompare(nameB);
+    });
+
+    // Ora l'array 'data' è ordinato per il campo 'name' in ordine alfabetico crescente.
+    // Puoi fare ciò che vuoi con i dati ordinati, ad esempio visualizzarli a schermo o eseguire altre operazioni.
 }
+
+function orderPopolazione() {
+ 
+    arancio.sort(function (a, b) {
+        return a.population - b.population;
+    });
+
+    renderPage();
+        }
+    
